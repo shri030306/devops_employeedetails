bd0b1fedb1e9295a59234ca0e7a546966b3c88ba
shri030306 <shrilakshmigs03@gmail.com>
2025-11-15 12:29:17 +0530

    Update employeedetails.py

--- employeedetails.py
+++ employeedetails.py
@@ -1,9 +1,8 @@
 emp_id = "E101"
 name = "Shrilakshmi"
 designation = "Developer"
-salary = 75000
 print("Employee Details")
 print("ID         :", emp_id)
 print("Name       :", name)
 print("Designation:", designation)
-print("Salary     : ₹", salary)
+
