c744a4121bc17091428ecde0bedd5519a67759d2
shri030306 <shrilakshmigs03@gmail.com>
2025-11-15 12:25:45 +0530

    Create employeedetails.py

---
+++ employeedetails.py
@@ -0,0 +1,9 @@
+emp_id = "E101"
+name = "Shrilakshmi"
+designation = "Developer"
+salary = 75000
+print("Employee Details")
+print("ID         :", emp_id)
+print("Name       :", name)
+print("Designation:", designation)
+print("Salary     : ₹", salary)
